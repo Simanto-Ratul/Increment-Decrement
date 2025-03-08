bc0a1a84a5f1700e2c1e48c1b00c2ba77848ee06
Simanto Saha <sahasimanto.828@gmail.com>
2025-03-08 21:21:05 +0600

    code by simanto

---
+++ Decrement.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Decrement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a : ");
+        int a = sc.nextInt();
+        System.out.print("Enter b : ");
+        int b = sc.nextInt();
+
+        b = --a;
+        System.out.println("Pre Decrement(a) : " + a);
+        System.out.println("Pre Decrement(b) : " + b);
+
+        System.out.print("Enter x : ");
+        int x = sc.nextInt();
+        System.out.print("Enter y : ");
+        int y = sc.nextInt();
+
+        y = x--;
+        System.out.println("Pre Decrement(x) : " + x);
+        System.out.println("Pre Decrement(y) : " + y);
+
+    }
+}
